d1fbb65c3c4f576218c7fa36b7c8526a61219e2b
William Parks <william.parks@gmail.com>
2020-05-09 23:09:24 -0700

    emacs init files all refreshed from scratch (mostly)

---
+++ custom.el
@@ -0,0 +1,46 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "d600c677f1777c1e4bfb066529b5b73c0179d0499dd4ffa3f599a0fb0cfbd501" "f9aede508e587fe21bcfc0a85e1ec7d27312d9587e686a6f5afdbb0d220eab50" default))
+ '(display-line-numbers-grow-only t)
+ '(display-line-numbers-width-start t)
+ '(hl-todo-keyword-faces
+   '(("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#4f97d7")
+     ("OKAY" . "#4f97d7")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#86dc2f")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX+" . "#dc752f")
+     ("\\?\\?\\?+" . "#dc752f")))
+ '(package-selected-packages
+   '(highlight-indent-guides xterm-color rustic use-package spacemacs-theme omtose-phellack-theme monokai-theme color-theme-modern))
+ '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e"))
+ '(rustic-lsp-server 'rust-analyzer t)
+ '(whitespace-display-mappings
+   '((space-mark 32
+		 [183]
+		 [46])
+     (newline-mark 10
+		   [182 10])
+     (tab-mark 9
+	       [8677 9]
+	       [92 9])))
+ '(whitespace-style
+   '(face trailing tabs spaces newline empty space-mark tab-mark newline-mark)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-newline ((t (:bold t :foreground "#292b2e" :background nil)))))
--- init.el
+++ init.el
@@ -1,132 +1,157 @@
-;; Setup backup files into a separate directory
-(setq inhibit-splash-screen t)
-(tooltip-mode -1)
-(when window-system
-  (scroll-bar-mode -1)
-  (tool-bar-mode -1)
-)
-
-;; Dealing with difference of terminal vs GUI display
-(if (display-graphic-p)
-    ;; When in graphic display
-    (progn
-      (tool-bar-mode -1)
-      ;; set size parameters of default frame
-      (add-to-list 'default-frame-alist '(left . 80))
-      (add-to-list 'default-frame-alist '(top . 2))
-      (add-to-list 'default-frame-alist '(height . 60))
-      ;;(add-to-list 'default-frame-alist '(width . 80))
-      ;; Sets fringe to l and r pixel large for each side
-      ;;(set-fringe-mode '(1 . 1))
-      (set-fringe-mode '(1 . 2))
-      )
-  ;; When in terminal
-  )
-
-(setq
- backup-by-copying t
- backup-directory-alist '(("." . "~/.emacs-backup"))
- delete-old-versions t
- kept-new-versions 6
- kept-old-versions 2
- version-control t
- )
+;;;; init.el -*- lexical-binding: t; -*-
 
 ;; Package setup
-(defvar package-archives)
+(require 'package)
+(setq package-enable-at-startup nil)
 (setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
+      '(
+        ;("melpa-stable" . "https://stable.melpa.org/packages/")
         ("melpa" . "https://melpa.org/packages/")
-       ))
+        ("gnu" . "https://elpa.gnu.org/packages/")
+        )
+      )
 (package-initialize)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package)
-)
-(eval-when-compile
+  )
+;(eval-when-compile
   (require 'use-package)
-  (setq use-package-always-ensure t)
-)
+  ;(setq use-package-always-ensure t)
+  ;(setq use-package-always-defer t)
+  ;)
 
-(when window-system
-  (use-package frame
-    :ensure nil
-    :config
-    (setq window-divider-default-right-width 1)
-    (window-divider-mode 1)
-    (set-face-attribute
-     'window-divider nil
-     :foreground (face-attribute
-                  'mode-line-inactive :background))))
+;; Customize system
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
 
+;; Window system
+(when (display-graphic-p)
+  (setq inhibit-splash-screen t)
+  (setq inhibit-startup-message t)
+  (tooltip-mode -1)
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1)
+  )
+
+;; Editor Backups and Autosaves
+(let ((backup-dir (expand-file-name "tmp/backups" user-emacs-directory))
+      (auto-saves-dir (expand-file-name "tmp/auto-saves" user-emacs-directory)))
+  (dolist (dir (list backup-dir auto-saves-dir))
+    (when (not (file-directory-p dir))
+      (make-directory dir t)))
+  (setq backup-directory-alist `(("." . ,backup-dir))
+        auto-save-file-name-transforms `((".*" ,auto-saves-dir t))
+        auto-save-list-file-prefix (concat auto-saves-dir ".saves-")
+        tramp-backup-directory-alist `((".*" . ,backup-dir))
+        tramp-auto-save-directory auto-saves-dir))
+(setq
+ backup-by-copying t ;; Don't delink hardlinks
+ delete-old-versions t ;; Clean up the backups
+ version-control t ;; Use version numbers on backups,
+ kept-new-versions 5 ;; keep some new versions
+ kept-old-versions 2 ;; and some old ones, too
+ )
+
+;; Basic
+(setq
+ column-number-mode t
+ line-number-mode t
+ visible-bell t ;; No noise pls
+ create-lockfiles nil ;; Get rid of creating .#* files in same directory
+ )
+(setq-default indent-tab-mode nil)
+
+;; Theme
+(use-package spacemacs-theme
+  :defer t
+  :init
+  (load-theme 'spacemacs-dark t)
+  )
+(use-package monokai-theme
+  :defer t
+  :init
+;  (load-theme 'monokai t)
+  )
+(use-package omtose-phellack-theme
+  :defer t
+  :init
+;  (load-theme 'omtose-darker t)
+  )
+
+;; Line Numbers
+(use-package display-line-numbers
+  :defer t
+  :hook (prog-mode . display-line-numbers-mode)
+  :custom
+  (display-line-numbers-grow-only t)
+  (display-line-numbers-width-start t)
+  )
 
 ;; Whitespace
-;; lines lines-tail newline trailing space-before-tab space-afte-tab empty
-;; indentation-space indentation indentation-tab tabs spaces
 (use-package whitespace
-  :config
-  (setq whitespace-style
-        '(
-          face
-          trailing
-          spaces
-          tabs
-          empty
-          newline
-          space-mark
-          tab-mark
-          newline-mark
-          ))
-  (setq whitespace-display-mappings
-        ;; all numbers are Unicode codepoint in decimal. try (insert-char 182) to see it
-        '(
-          (space-mark 32 [183] [46]) ;; 32 SPACE, 183 MIDDLE DOT, 46 FULL STOP
-          (newline-mark 10 [182 10]) ;; 10 LINE FEED
-          ;;(tab-mark 9 [9655 9] [92 9]) ;; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE
-          (tab-mark 9 [8677 9] [92 9]) ;; 9 TAB, 8677 RIGHTWARDS ARROW TO BAR
-          ))
-  (if (>= emacs-major-version 23)
-      (add-hook 'after-change-major-mode-hook 'whitespace-mode)
-    (add-hook 'after-change-major-mode-hook 'whitespace-global-mode)
-    )
+  :defer t
+  :hook
+  (prog-mode . whitespace-mode)
+  (text-mode . whitespace-mode)
+  :custom
+  (whitespace-style
+   '(
+     face
+     trailing
+     tabs
+     spaces
+     newline
+     empty
+     space-mark
+     tab-mark
+     newline-mark
+     ))
+  (whitespace-display-mappings
+   ;; all numbers are Unicode codepoint in decimal. try (insert-char 182) to see it
+   '(
+     (space-mark 32 [183] [46]) ;; 32 SPACE, 183 MIDDLE DOT, 46 FULL STOP
+     (newline-mark 10 [182 10]) ;; 10 LINE FEED
+     (tab-mark 9 [8677 9] [92 9]) ;;9 TAB, 8677 RIGHTWARDS ARROW TO BAR
+     ))
   )
 
-;;(defvar mybgcolor "#181a26") ;; deeper-blue theme background
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(whitespace-empty ((t (:background "#181a26"))))
- '(whitespace-indentation ((t (:foreground "firebrick" :background "beige"))))
- '(whitespace-newline ((t (:bold t :foreground "#181a26" :background "#181a26"))))
- '(whitespace-space ((t (:bold t :foreground "#181a26" :background "#181a26"))))
- '(whitespace-tab ((t (:underline t :foreground "LightSkyBlue"))))
- '(whitespace-trailing ((t (:underline t :foreground "DeepPink")))))
+;; TODO: Indentation
+;(use-package highlight-indent-guides
+;  :defer t
+;  :hook (prog-mode . highlight-indent-guides)
+;  :custom
+;  (highlight-indent-guides-method 'bitmap)
+;  )
 
-
-(use-package display-line-numbers
-  :ensure nil
-  :config
-  (setq display-line-numbers-grow-only t
-        display-line-numbers-width-start t))
-
-;; Load default theme
-(if (>= emacs-major-version 23)
-    (add-hook 'after-init-hook
-              (lambda()
-                ;;(load-theme 'dark-mint)
-                ;;(load-theme 'kooten t)
-                ;;(load-theme 'zenbun t)
-                ;;(load-theme 'solarized-dark)
-                ;;(load-theme 'monokai t)
-                ;;(load-theme 'cyberpunk t)
-                ;;(load-theme 'ample t)
-                (load-theme 'deeper-blue)
-                ;;(load-theme 'tango-dark)
-                )
-              )
+;; Programming modes
+(use-package prog-mode
+  :defer t
+  :hook (prog-mode . show-paren-mode)
   )
+
+;; Lisp Mode
+(use-package elisp-mode
+  :defer t
+  :mode ("\\.el\\'" . 'emacs-lisp-mode)
+  :config
+  (setq indent-tabs-mode nil)
+  :hook (emacs-lisp-mode . eldoc-mode)
+  )
+
+;; Rust Mode (Rustic)
+;(use-package rustic
+;  :ensure t
+;  :custom
+;  (rustic-lsp-server 'rust-analyzer)
+;  )
+
+;; TODO: C/C++ Mode
+;; TODO: Python Moden
+;; TODO: Json Mode
+;; TODO: Yaml Mode
+;; TODO: Golang Mode
+;; TODO: Markdown Mode
 
 (provide 'init)
 ;;; init.el ends here
