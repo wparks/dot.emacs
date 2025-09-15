0303324847428cf65c27e7952203c0862f124974
wparks <william.parks@gmail.com>
2025-09-15 13:03:22 -0700

    Simplify backup directory creation
    Remove unnecessary checks - make-directory handles existing directories gracefully

--- init.el
+++ init.el
@@ -32,9 +32,8 @@
 ;; Editor backups and autosaves
 (let ((backup-dir (expand-file-name "tmp/backups" user-emacs-directory))
       (auto-saves-dir (expand-file-name "tmp/auto-saves" user-emacs-directory)))
-  (dolist (dir (list backup-dir auto-saves-dir))
-    (when (not (file-directory-p dir))
-      (make-directory dir t)))
+  (make-directory backup-dir t)
+  (make-directory auto-saves-dir t)
   (setq backup-directory-alist `(("." . ,backup-dir))
         auto-save-file-name-transforms `((".*" ,auto-saves-dir t))
         auto-save-list-file-prefix (concat auto-saves-dir ".saves-")
